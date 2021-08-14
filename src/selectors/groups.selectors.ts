9e2114242193691f8b66260fbc82e7a897ca5d98
Mitali <varshneymitali8122gmail.com>
2021-08-14 20:05:52 +1200

    update groups selector

--- src/selectors/groups.selectors.ts
+++ src/selectors/groups.selectors.ts
@@ -1,56 +1,38 @@
+
 import { createSelector } from "reselect";
 import { groupStateSelector } from "./app.selectors";
 
-
-//3 ways of writing selector
-
-//export const groupQuerySelector = (state:AppState) => state.groups.query;   //1 approach   always  run the commplete code--> its disadvantage
-
-/*export const groupQuerySelector = (state: AppState) => {  //2 approach
-    const groupState = groupStateSelector(state);
-    return groupState.query;
-}*/
-
-export const groupQuerySelector = createSelector([groupStateSelector],    //3 approach
-    (groupState) => groupState.query);
-
-
-/*export const groupQueryMapSelector = (state: AppState) => {
-    const groupState = groupStateSelector(state);
-    return groupState.queryMap;
-}*/
-
-export const groupQueryMapSelector = createSelector([groupStateSelector],
-    (groupState) => groupState.queryMap);
-
-
-/*export const groupByIdSelector = (state: AppState) => {
-    const groupState = groupStateSelector(state);
-    return groupState.byId;
-}*/
-const groupByIdSelector = createSelector([groupStateSelector],    //3 approach
-    (groupState) => groupState.byId);
-
-const groupLoadingQuerySelector = createSelector([groupStateSelector], (groupState) =>
-    groupState.loadingQuery
+export const groupQuerySelector =  createSelector(
+    [groupStateSelector],
+    (groupState) => groupState.query
 )
-export const groupLoadingSelector = createSelector([groupQuerySelector, groupLoadingQuerySelector],
-    (query, loadingMap) => loadingMap[query]);
 
-/*export const groupsSelector = (state: AppState) => {
-    const query = groupQuerySelector(state);
-    const queryMap = groupQueryMapSelector(state);
-    const byId = groupByIdSelector(state);
-    const groupIds = queryMap[query] || [];
-    const groups = groupIds.map((id) => byId[id]);
-    return groups;
+export const groupQueryMapSelector =  createSelector(
+    [groupStateSelector],
+    (groupState) => groupState.queryMap
+)
+
+export const groupByIdSelector =  createSelector(
+    [groupStateSelector],
+    (groupState) => groupState.byId
+)
 
 
-}*/
-export const groupsSelector = createSelector([groupQuerySelector, groupByIdSelector, groupQueryMapSelector],
-    (query, byId, queryMap) => {
+export const groupsLoadingSelector =  createSelector(
+    [groupStateSelector],
+    (groupState) => groupState.loading
+)
+
+/*
+export const groupLoadingSelector =  createSelector(
+    [groupQuerySelector , groupQueryLoadingSelector],
+    (query , loadingMap) => loadingMap[query]
+)
+*/
+export const groupsSelector =  createSelector(
+    [groupQuerySelector , groupByIdSelector , groupQueryMapSelector],
+    (query , byId , queryMap) => {
         const groupIds = queryMap[query] || [];
         const groups = groupIds.map((id) => byId[id]);
-        return groups
-    });
-
+        return groups;
+    })
