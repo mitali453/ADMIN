bfb3a5264a728a493bfd5b3c3b0d74296121963f
Mitali <varshneymitali8122gmail.com>
2021-08-14 20:00:29 +1200

    change in avatar

--- src/components/Avatars/Avatar.tsx
+++ src/components/Avatars/Avatar.tsx
@@ -1,7 +1,7 @@
 import {ButtonHTMLAttributes, FC, memo} from "react";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-    theme : 'active' | 'inactive';
+    theme ?: 'active' | 'inactive';
     imageUrl: string;
 }
 const Avatar: FC<Props> = ({ theme,imageUrl, className}) =>{
@@ -10,7 +10,10 @@ const Avatar: FC<Props> = ({ theme,imageUrl, className}) =>{
 
 return (
     <div className="relative inline-block">
-        <img className="inline-block object-cover w-32 h-32 rounded-full" src={imageUrl} alt="Profile image"/>
+        <img className="inline-block object-cover w-16 h-16 rounded-full" 
+        src={imageUrl} 
+        onError={(event: any) => { event.target.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTo8ot81o5G-DZLv5GatY5S5Jsc2rHobmXkQ&usqp=CAU"; }} 
+        alt="Profile image"/>
         <span className={"absolute bottom-4 right-0 inline-block w-4 h-4  border-2 border-white rounded-full "+ themeClasses +" "+ className}></span>
   </div>
 );
