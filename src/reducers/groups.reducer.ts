e926e4dfcfdfa7d3f342864597525149a940e95d
Mitali <varshneymitali8122gmail.com>
2021-08-14 20:04:10 +1200

    some change

--- src/reducers/groups.reducer.ts
+++ src/reducers/groups.reducer.ts
@@ -1,41 +1,43 @@
 import { Reducer } from "redux";
 import { GROUPS_QUERY, GROUPS_QUERY_COMPLETED } from "../actions/actions.constants";
+
 import { Group } from "../models/Group";
 import { addMany, EntityState, getIds } from "./entity.reducer";
 
-export interface GroupState extends EntityState<Group> {
-    query: string;
-    loadingQuery: { [query: string]: boolean };
-    queryMap: { [query: string]: number[] }
+export interface GroupState extends EntityState<Group>{ 
+    query : string;
+    loading :  boolean 
+    queryMap : { [query : string] : number[] }
 }
 const initialState = {
-    byId: {},
-    query: "",
-    queryMap: {},
-    loadingQuery: {},
+    byId : {},
+    query : "",
+    queryMap : {},
+    loading : false
 };
 
-export const groupReducer: Reducer<GroupState> = (state = initialState, action) => {
-    switch (action.type) {
+export const groupReducer : Reducer<GroupState> = (state = initialState ,action) => {
+    switch(action.type){
         case GROUPS_QUERY:
-            const {query,loading}=action.payload as {
-                query:string,
-                loading:boolean,
+            const query = action.payload
+            return { 
+            ...state ,
+            query: query,
+            loading : true
             };
-            return { ...state, query: query, loadingQuery: { ...state.loadingQuery, [query]: loading } };
         case GROUPS_QUERY_COMPLETED:
             const groups = action.payload.groups as Group[];
+
             const groupIds = getIds(groups);
 
-            const newState = addMany(state, groups) as GroupState;
+            const newState = addMany(state , groups) as GroupState;
 
             return {
                 ...newState,
-                queryMap: { ...newState.queryMap, [action.payload.query]: groupIds },
-                loadingQuery: { ...newState.loadingQuery, [action.payload.query]: false }
-                //byId: { ...state.byId, ...groupMap },
+                queryMap :{...newState.queryMap , [action.payload.query] : groupIds },
+                loading : false 
             }
         default:
-            return state;
+            return state
     }
 }
