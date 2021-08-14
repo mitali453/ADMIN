e0056c88acb262775a7166deda2980f8e55707bf
Mitali <varshneymitali8122gmail.com>
2021-08-14 20:01:23 +1200

    changed in dashboard page

--- src/pages/AppContainer/Dashboard.page.tsx
+++ src/pages/AppContainer/Dashboard.page.tsx
@@ -1,46 +1,18 @@
-import { FC, memo } from "react";
-import { FaSpinner } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { fetchGroups } from "../../middleware/groups.middleware";
-import { groupLoadingSelector, groupQuerySelector, groupsSelector } from "../../selectors/groups.selectors";
+import React from "react";
+import { meSelector } from "../../selectors/auth.selectors";
 import { useAppSelector } from "../../store";
 
-interface Props { }
-
-const Dashboard: FC<Props> = () => {
-
-    const query = useAppSelector(groupQuerySelector);
-    const loading = useAppSelector(groupLoadingSelector);
-    const groups = useAppSelector(groupsSelector);
-
+interface Props{}
+const Dashboard: React.FC<Props> = (props) => {
+    const user = useAppSelector(meSelector);
     return (
-        <div className=" p-4" >
-            <h1 className=" text-red-500 font-semibold text-2xl p-2">this is Dashboard page</h1>
-            <br />
-            <div className=" flex space-x-4">
-                <input
-                    className="border border-black" type="text"
-                    value={query}
-                    placeholder="Search"
-                    onChange={(e) => {
-                        fetchGroups({ query: e.target.value, status: "all-groups" });
-                    }} />
-                {loading && <FaSpinner className=" text-2xl animate-spin"></FaSpinner>}
-            </div>
-            <div>
-                {
-                    groups.map((group) => (
-                        <div >{group.name}</div>
-                    ))
-                }
-                {!loading && groups.length===0 && "No data found"}
-            </div>
-
-            <Link to="/recordings"><span className=" text-blue-500">Go to recording</span></Link>
+        <div>This is dashboard page
+           
+           <div className=" text-yellow-400">{user!.first_name}</div>
+            
         </div>
     );
 };
-
-Dashboard.defaultProps = {}
-
-export default memo(Dashboard);
+Dashboard.defaultProps = {
+}
+export default Dashboard;
