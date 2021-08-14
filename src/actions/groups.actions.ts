ab0adc5c78c76953f374c35832c27fef50c0d4d7
Mitali <varshneymitali8122gmail.com>
2021-08-14 20:06:43 +1200

    deleted group action created with bindCreaters

--- src/actions/groups.actions.ts
+++ src/actions/groups.actions.ts
@@ -1,17 +1,10 @@
-import { bindActionCreators } from "redux";
-import { Group } from "../models/Group";
-import { store } from "../store";
-import { GROUPS_QUERY, GROUPS_QUERY_COMPLETED } from "./actions.constants";
+import { Group } from "../models/Group"
+import { GROUPS_QUERY, GROUPS_QUERY_COMPLETED } from "./actions.constants"
 
 
-const queryAction = (query: string ,loading:boolean) => ({ type: GROUPS_QUERY, payload:{ query,loading} });
+export const groupQueryAction = (query : string) => ({ type : GROUPS_QUERY , payload : query })
 
-const queryCompletedAction = (query: string, groups: Group[]) => ({ type: GROUPS_QUERY_COMPLETED, payload: { query, groups }, });
-export { };
+export const groupQueryCompletedAction = (query : string , groups : Group[]) => ({ type : GROUPS_QUERY_COMPLETED , payload : {query , groups}  })
 
 
 
-export const groupsActions = bindActionCreators({
-    query: queryAction,
-    queryCompleted:queryCompletedAction
-}, store.dispatch)
