87aab0927572111d63428251c30010345d63cf40
Mitali <varshneymitali8122gmail.com>
2021-08-15 20:31:52 +1200

    Added some css

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -6,9 +6,9 @@ interface Props{
 }
 const Icon: FC<Props> = (props) =>{
 return (
- <div className=" bg-gray-300 h-28 p-5 border border-gray-800 flex flex-col">
-     <Link className=" text-lg font-semibold "  to="/editProfile">Edit Profile</Link>
-  <button className=" text-lg font-semibold text-gray-800 border-0 p-2" onClick={
+ <div className=" bg-gray-300 h-28 p-5 border border-gray-800 flex flex-col ">
+     <Link className=" text-lg font-semibold hover:bg-white p-2 pt-1 rounded-md "  to="/editProfile">Edit Profile</Link>
+  <button className=" text-lg font-semibold hover:bg-white text-gray-800 border-0 p-2 rounded-md" onClick={
                         () => {
                             logout();
                             window.location.href = "/login";
