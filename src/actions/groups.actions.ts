c92096eb9d900d4715b6522e9a4576ba6f01ba1e
Mitali <varshneymitali8122gmail.com>
2021-08-16 22:18:33 +1200

    create Fetching actions for one group

--- src/actions/groups.actions.ts
+++ src/actions/groups.actions.ts
@@ -1,10 +1,18 @@
+
 import { Group } from "../models/Group"
-import { GROUPS_QUERY, GROUPS_QUERY_COMPLETED } from "./actions.constants"
+import { GROUPS_FETCH_ONE, GROUPS_FETCH_ONE_COMPLETE, GROUPS_QUERY, GROUPS_QUERY_COMPLETED } from "./actions.constants"
 
 
 export const groupQueryAction = (query : string) => ({ type : GROUPS_QUERY , payload : query })
 
 export const groupQueryCompletedAction = (query : string , groups : Group[]) => ({ type : GROUPS_QUERY_COMPLETED , payload : {query , groups}  })
 
+export const fetchOneGroup=( id:number)=>({
+    type:GROUPS_FETCH_ONE,
+    payload:id,
+})
 
-
+export const fetchOneGroupComplete=( group:Group)=>({
+    type:GROUPS_FETCH_ONE_COMPLETE,
+    payload: group,
+})
