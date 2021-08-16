2fb7a0412711df39cbc7266b945510546c1a0600
Mitali <varshneymitali8122gmail.com>
2021-08-16 22:20:07 +1200

    use of selectors to prevent unnecessary rendring

--- src/pages/AppContainer/GroupDetail.page.tsx
+++ src/pages/AppContainer/GroupDetail.page.tsx
@@ -1,11 +1,30 @@
-import { FC, memo } from "react";
+import { FC, memo, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { fetchOneGroup } from "../../actions/groups.actions";
+import { selectedGroupSelector } from "../../selectors/groups.selectors";
+import {useAppSelector} from "../../store"
 interface Props {
 }
 const GroupDetail: FC<Props> = (props) => {
-    const { groupId } = useParams < {groupId:any} > ();
+    const  groupId  = +useParams<{groupId:string}>().groupId;
+    const group= useAppSelector(selectedGroupSelector);
+    
+
+    const dispatch= useDispatch();
+    useEffect(()=>{
+        dispatch(fetchOneGroup(groupId));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[groupId]);
+
+    if(!group){
+        return <div>Loading group...</div>
+    }
     return (
-        <div>This is a detail page of {groupId}</div>
+        <div>This is a detail page of {groupId } {group.name}
+        <p>{group.description}</p>
+        </div>
     );
 };
 GroupDetail.defaultProps = {
