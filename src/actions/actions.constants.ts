da49764a8bccdaddf3b65c1b8ed6e74c6a246054
Mitali <varshneymitali8122gmail.com>
2021-08-16 21:45:47 +1200

    add the constants

--- src/actions/actions.constants.ts
+++ src/actions/actions.constants.ts
@@ -5,4 +5,6 @@ export const ME_LOGIN = "me/login";
 
 //GROUPS CONSTANTS
 export const GROUPS_QUERY = "groups/query";
+export const GROUPS_FETCH_ONE="groups/fetch_one";
+export const GROUPS_FETCH_ONE_COMPLETE="groups/fetch_one_complete";
 export const GROUPS_QUERY_COMPLETED = "groups/query_completed";
